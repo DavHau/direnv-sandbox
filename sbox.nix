8fa3822dd4aef16a6d758272a837277d24bf6417
DavHau <hsngrmpf+github@gmail.com>
2026-03-15 21:56:54 +0800

    sbox: bind /etc/fonts
    some programs crash if this doesn't exist

--- sbox.nix
+++ sbox.nix
@@ -253,6 +253,7 @@ let
       --ro-bind-try /etc/zshrc /etc/zshrc \
       --ro-bind-try /etc/zsh /etc/zsh \
       --ro-bind-try /etc/fish /etc/fish \
+      --ro-bind-try /etc/fonts /etc/fonts \
       --ro-bind /etc/nix /etc/nix \
       --ro-bind-try /etc/static /etc/static \
       --ro-bind "$RESOLV" /etc/resolv.conf \
